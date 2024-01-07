f1fb58691382ef5d2d952059ad031df26d0a0204
VanHoang610 <137982306+VanHoang610@users.noreply.github.com>
2024-01-08 00:03:59 +0700

    firts-commit

---
+++ App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+
+
+function App(): React.JSX.Element {
+  return (
+    <SafeAreaView>
+      <Text>Hello</Text>
+    </SafeAreaView>
+  );
+}
+
+export default App;
